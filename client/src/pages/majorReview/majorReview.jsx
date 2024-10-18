88ae68590e34be18319d59c52a5e7354d622d811
Nithi Pipatkittikul <fordpipatkitkul@gmail.com>
2024-10-17 22:53:24 -0500

    ss

--- client/src/pages/majorReview/majorReview.jsx
+++ client/src/pages/majorReview/majorReview.jsx
@@ -45,7 +45,7 @@ function MajorReview() {
               reviews.map((review, index) => (
                 <div key={`${review.reviewId}-${index}`} className='review'>
                       <div className='bottom-top'>
-                          <h3>Name: {review.user.name || anonymous}</h3>
+                          <h3>{review.user.name || anonymous}</h3>
                           <h3><span>QUALITY: </span>{review.quality}/5</h3>
                           <h3><span>DIFFICULT: </span>{review.difficulty}/5</h3>
                       </div>
