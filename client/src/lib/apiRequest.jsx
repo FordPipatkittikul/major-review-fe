9d8d267048b3a18acd31a16eb990fd275840ce6e
Nithi Pipatkittikul <fordpipatkitkul@gmail.com>
2024-10-23 21:05:35 -0500

    adding Basic Auth headers

--- client/src/lib/apiRequest.jsx
+++ client/src/lib/apiRequest.jsx
@@ -1,8 +1,15 @@
 import axios from "axios";
 
 
+const username = import.meta.env.VITE_SECURITY_USERNAME;
+const password = import.meta.env.VITE_SECURITY_PASS;
+const token = btoa(`${username}:${password}`);
+
 const apiRequest = axios.create({
     baseURL: "http://localhost:8080/api",
+    headers: {
+        'Authorization': `Basic ${token}`
+    },
     withCredentials: true
 });
 
